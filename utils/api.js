67a0eeb4ddc1107be63597a2454c84640cb96471
amaraa <oamraa62@gmail.com>
2024-07-10 23:37:36 +0800

    new version

--- utils/api.js
+++ utils/api.js
@@ -5,7 +5,7 @@ const TIMEOUT = 20000;
 export function weatherRequest() {
   let query = {
     baseURL:
-      "http://api.weatherapi.com/v1/current.json?key=3ad40bf8acfd4609881150536240607&q=Ulaanbaatar&aqi=no",
+      "https://api.weatherapi.com/v1/current.json?key=3ad40bf8acfd4609881150536240607&q=Ulaanbaatar&aqi=no",
     timeout: TIMEOUT,
   };
 
